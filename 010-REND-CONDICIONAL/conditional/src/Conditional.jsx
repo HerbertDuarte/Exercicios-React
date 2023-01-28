54d1aa9fcc637c55b12559013bea41d3c3e9de9f
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-01-27 21:51:39 -0300

    Update Conditional.jsx

--- 010-REND-CONDICIONAL/conditional/src/Conditional.jsx
+++ 010-REND-CONDICIONAL/conditional/src/Conditional.jsx
@@ -1,10 +1,14 @@
-import {useState} from 'react'
-
 const Conditional = () => {
+
+  const x = false
+  const y = true
   return (
     <div>
-      <p>Usando ternário</p>
-      {}
+      <h2>Condicional &&</h2>
+      {x && <p>renderização true</p>}
+      {!x && <p>renderização false</p>}
+      <h2>Usando ternário</h2>
+      {y == true ? <p>ternário True</p> : <p>ternário False</p>}
     </div>
   )
 }
