e473b66a86a8019679d605775df19ee6c5967fb3
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-02-13 00:35:41 -0300

    css in react

---
+++ 013-CSS-IN-REACT/css in react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+        <h1>CSS no React</h1>
+        <ul>
+          <li>CSS components = css para cada component porém devem ser estabelecidas classes para que o css não vaze</li>
+          <li>CSS inline = Deve se atentar de por os estilos entre {`{}`} e em camelCase </li>
+        </ul>
+    </div>
+  )
+}
+
+export default App
