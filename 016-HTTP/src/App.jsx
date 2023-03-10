5186de5ca9bc2e2a07f8ca8ee64e93aa40b0fd1a
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-03-10 00:37:51 -0300

    http updt

--- 016-HTTP/src/App.jsx
+++ 016-HTTP/src/App.jsx
@@ -1,31 +1,85 @@
-import './App.css'
-import { useState, useEffect } from 'react'
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  const url = 'http://localhost:3000/products'
+  const url = "http://localhost:3000/products";
 
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
-  useEffect(()=>{
-    async function fetchApi(){
-      const res = await fetch(url)
-      const data = await res.json()
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const newProduct = {
+      name,
+      price
+    }
+    console.log(newProduct)
+    console.log(products)
 
-      setProducts(data)
+    await fetch( url, {
+      method: 'POST',
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(newProduct)
+    })
+
+  };
+
+  // 1 - fecth dos dadosda API
+
+  useEffect(() => {
+    async function fetchApi() {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setProducts(data);
     }
     fetchApi();
-  }, [])
+  }, []);
 
-  console.log(products) 
+  // 2 - Add products
+
+  console.log(products);
 
   return (
     <div className="App">
       <h1>Produtos</h1>
-      {products.map((product)=>(
-        <p key={product.id}>Produto : {product.name} - preço : R${product.price}</p>
+      {products.map((product) => (
+        <p key={product.id}>
+          Produto : {product.name} - preço : R${" "}
+          {product.price.toLocaleString("pt-br", { minimumFractionDigits: 2 })}
+        </p>
       ))}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome do Produto
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+          />
+        </label>
+
+        <label>
+          Preço
+          <input
+            onChange={(e)=> setPrice(Number(e.target.value))}
+            type="number"
+            name="price"
+            id="price"
+            value={price}
+          />
+        </label>
+
+        <input type="submit" value="Add" />
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
